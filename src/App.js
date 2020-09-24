e53c923733d85baf77129af25b9b79eb653807cc
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-09-24 16:09:49 +1000

    Set up Nav Bar with Basic Styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import "./App.css"
 import NavBar from "./NavBar.js"
 import Content from "./Content.js"
 import Profile from "./Profile.js"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
     return (
-        <div>
-            <NavBar />
-            <Content />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <NavBar />
+                </Route>
+                <Route path="/explore">
+                    <NavBar />
+                </Route>
+                <Route path="/profile/:username">
+                    <NavBar />
+                </Route>
+                <Route path="/login">
+                    <NavBar />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -6,28 +6,29 @@ import {
     Link,
     useParams,
 } from "react-router-dom"
+import BNav from "react-bootstrap/Nav"
+import "./style/NavBar.css"
 
 const NavBar = (props) => {
     return (
         //ADD LOGO ON THE LEFT
         //ADD SEARCH BAR ON THE LEFT
-        <Router>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/explore">Explore</Link>
-                <Link to="/profile/:username">Profile</Link>
-                <Link to="/login">Login</Link>
-            </div>
-
-            <Switch>
-                <Route path="/">{/* TODO: Home page component */}</Route>
-                <Route path="/explore">{/* TODO: Explore all tweets */}</Route>
-                <Route path="/profile/:username">
-                    {/* TODO: Individual profile feed */}
-                </Route>
-                <Route path="/login">{/* TODO: Login page */}</Route>
-            </Switch>
-        </Router>
+        <BNav variant="tabs" activeKey="/home">
+            <BNav.Item>
+                <BNav.Link class="float-right" href="/">
+                    Home
+                </BNav.Link>
+            </BNav.Item>
+            <BNav.Item>
+                <BNav.Link href="/explore">Explore</BNav.Link>
+            </BNav.Item>
+            <BNav.Item>
+                <BNav.Link href="/profile/:username">Profile</BNav.Link>
+            </BNav.Item>
+            <BNav.Item>
+                <BNav.Link href="/login">Login</BNav.Link>
+            </BNav.Item>
+        </BNav>
     )
 }
 
