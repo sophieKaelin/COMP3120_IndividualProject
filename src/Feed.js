3b39848f8dc53e7f626f53cc6b66ae6ab6b82c34
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-09-25 13:36:31 +1000

    basic feed working

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,6 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { defaultModifiers } from "@popperjs/core/lib/popper-lite"
 
 const Feed = ({ feed }) => {
     //Will have many modes:
@@ -6,8 +8,26 @@ const Feed = ({ feed }) => {
     //2. show posts user is following
     //3. show posts by user
     //4. show posts mentioning user (HD)
-    console.log("MODE IS: ", feed)
-    return <h2>"HELLO WORLD"</h2>
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/posts").then((response) => {
+            setPosts(response.data)
+            console.log("yeeeehawwww")
+            console.log(response.data)
+        })
+    }, [])
+
+    console.log(posts)
+    return posts.map((data) => (
+        <div>
+            <h2>@{data.user}</h2>
+            <p>{data.content}</p>
+            <p>
+                <i>Likes: {data.likes.length}</i>
+            </p>
+        </div>
+    ))
 }
 
 export default Feed
