4b1f355028d558c54152bdb59f93930a625eb7c7
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-09-27 18:00:52 +1000

    fix heroku routing issue

--- server/server.js
+++ server/server.js
@@ -167,6 +167,10 @@ app.post("/api/login", async (request, response) => {
     }
 })
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"))
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
